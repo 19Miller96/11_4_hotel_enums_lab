3774500fe69145362dfdac11cf795eb5f85dd922
m6ttw <matthewrhyswilliams@live.com>
2021-02-11 12:14:51 +0000

    room tests pass

---
+++ src/main/java/rooms/Room.java
@@ -0,0 +1,29 @@
+package rooms;
+
+import guest.Guest;
+
+import java.util.ArrayList;
+
+public class Room {
+
+    private int capacity;
+    private ArrayList<Guest> guests;
+
+    public Room(int capacity){
+        this.capacity = capacity;
+        this.guests = new ArrayList<Guest>();
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public int countGuestsList(){
+        return this.guests.size();
+    }
+
+    public void addToGuestsList(Guest guest) {
+        this.guests.add(guest);
+    }
+
+}
---
+++ src/test/java/RoomTest.java
@@ -0,0 +1,30 @@
+import guest.Guest;
+import org.junit.Before;
+import org.junit.Test;
+import rooms.Room;
+
+import static org.junit.Assert.assertEquals;
+
+public class RoomTest {
+
+    Room room;
+    Guest guest;
+
+    @Before
+    public void before(){
+        room = new Room(4);
+        guest = new Guest("Harry");
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(4, room.getCapacity());
+    }
+
+    @Test
+    public void canAddToGuestList(){
+        room.addToGuestsList(guest);
+        assertEquals( 1, room.countGuestsList());
+    }
+
+}
